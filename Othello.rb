156da895c6a17f415796714c301236c0e1f3cccf
jajapop <miki_yudai_19891111@yahoo.co.jp>
2020-07-21 00:16:59 +0900

    add_putable_method

--- Othello.rb
+++ Othello.rb
@@ -1,5 +1,5 @@
 class Board
-  attr_accessor :piece, :field, :change_color_stocks, :my_color, :enemy_color
+  attr_accessor :piece, :field, :my_color, :enemy_color, :empty_cells
   def initialize
     @field = Array.new(8){Array.new(8,:none)}
     field[3][3] = :black
@@ -8,20 +8,50 @@ class Board
     field[4][4] = :black
     @my_color = :black
     @enemy_color = :white
+    @empty_cells = []
+    for i in 0..8
+      for j in 0..8
+        empty_cells << [i,j]
+      end
+    end
+    empty_cells.remove_from_empty_cells(3,3)
+    empty_cells.remove_from_empty_cells(3,4)
+    empty_cells.remove_from_empty_cells(4,3)
+    empty_cells.remove_from_empty_cells(4,4)
   end
 
   def next_turn
     @my_color, @enemy_color = enemy_color, my_color
   end
 
+  Array.class_eval do
+    def remove_from_empty_cells(i,j)
+      self.delete([i,j])
+    end
+  end
+
   def put_piece(i,j)
     i -= 1
     j -= 1
-    field[i][j] = my_color
-    turn_pieces(i,j)
+    if putable?(i,j)
+      field[i][j] = my_color
+      turn_pieces(i,j)
+      empty_cells.remove_from_empty_cells(i,j)
+    else
+      puts "そこには置けません"
+    end
   end
 
   def turn_pieces(i,j)
+    stocks = gathering_turnable_pieces(i,j)
+    stocks.each do |a|
+      a.each_slice(2) do |i,j|
+        field[i][j] = my_color
+      end
+    end
+  end
+
+  def gathering_turnable_pieces(i,j)
     @change_color_stocks = []
     ary = []
     check_line(i,j,1,0,ary)
@@ -32,19 +62,13 @@ class Board
     check_line(i,j,1,-1,ary)
     check_line(i,j,-1,1,ary)
     check_line(i,j,-1,-1,ary)
-    change_color_stocks.each do |a|
-      a.each_slice(2) do |i,j|
-        field[i][j] = my_color
-      end
-    end
+    @change_color_stocks
   end
 
   def check_line(i,j,a,b,ary)
-    # ary << [i,j]
     return ary.clear if !within_range(i+a,j+b) || field[i+a][j+b] == :none
     if field[i+a][j+b] == my_color
       @change_color_stocks << ary.flatten
-      return change_color_stocks.length
     end
     ary << [i+a,j+b]
     check_line(i+a,j+b,a,b,ary)
@@ -52,6 +76,22 @@ class Board
 
   def within_range(i,j)
     i<8 && j<8 && i>=0 && j>=0
+  end
+
+  def putable?(i,j)
+    !!check_putable_cells.find_index([i,j])
+  end
+
+  def check_putable_cells
+    putable_cells = []
+    empty_cells.each do |i,j|
+      count = 0
+      gathering_turnable_pieces(i,j).each do |piece|
+        count += 1 unless piece.empty?
+      end
+      putable_cells << [i,j] if count > 0
+    end
+    putable_cells
   end
 
   def puts_field
