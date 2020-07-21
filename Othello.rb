ea9d9873b63fb09d374dda8fa825b60e6193c9fb
jajapop <miki_yudai_19891111@yahoo.co.jp>
2020-07-21 23:57:26 +0900

    add_ask_continue?_ask_retire?

--- Othello.rb
+++ Othello.rb
@@ -230,6 +230,7 @@ class Othello
     puts "どちらにしますか？番号を入力して下さい"
     puts "1 黒番, 2 白番"
     i = gets.to_i
+    puts "ゲームを終了したい場合は、endまたはexitと入力して下さい。"
     board.puts_field
     case i
     when 1
@@ -256,6 +257,22 @@ class Othello
     else
       puts "もう一度入力して下さい"
       ask_continue?
+    end
+  end
+
+  def ask_retire?
+    puts "本当にゲームを終了しますか？"
+    puts "1 はい, 2 いいえ (番号を入力して下さい)"
+    i = gets.to_i
+    case i
+    when 1
+      ask_continue?
+    when 2
+      board.puts_field
+      put_request
+    else
+      puts "もう一度入力して下さい"
+      ask_retire?
     end
   end
 
@@ -312,12 +329,13 @@ class Othello
 
   def put_request
     print "縦 横: "
-    i,j = gets.split.map(&:to_i)
+    i,j = gets.split
+    return ask_retire? if i == "end" || i == "exit"
     if i.nil? || j.nil?
       puts "もう一度入力して下さい"
       return put_request
     end
-    put_request unless board.put_piece(i,j)
+    put_request unless board.put_piece(i.to_i,j.to_i)
   end
 end
 
